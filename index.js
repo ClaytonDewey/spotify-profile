2a0d522fc0e5dcc7330371f437582dc394ae66ed
ClaytonDewey <clay@dryadmedia.com>
2022-08-25 20:18:58 -0500

    Set login requirements

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
+const axios = require("axios");
+const port = 8888;
 
-// app.METHOD(PATH, HANDLER)
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get("/", (req, res) => {
   const data = {
-    name: "Clayton",
+    name: "Hello",
     isAwesome: true,
   };
 
   res.json(data);
 });
 
-app.get("/awesome-generator", (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? "really" : "not"} awesome.`);
+/**
+ * Generates a random string containing numbers and letters
+ * @param {number} lengthThe length of the string
+ * @return { string} the generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const searchParams = new URLSearchParams({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  const queryParams = searchParams.toString();
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-const port = 8888;
+app.get("/callback", (req, res) => {
+  res.send("callback");
+});
+
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
+
+// const searchparams = new URLSearchParams({
+//   clien_id: CLIENT_ID,
+//   response_type: "code",
+//   redirect_uri: REDIRECT_URI,
+// });
+
+// const queryParams = searchparams.toString();
