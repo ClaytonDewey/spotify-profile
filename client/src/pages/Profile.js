4d95ce872daffac871151e9c4be1478240e7a05c
ClaytonDewey <clay@dryadmedia.com>
2022-08-28 11:10:02 -0500

    Add topTracks to Profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,14 +4,16 @@ import {
   getCurrentUserProfile,
   getCurrentUserPlaylists,
   getTopArtists,
+  getTopTracks,
 } from "../spotify";
-import { SectionWrapper, ArtistsGrid } from "../components";
+import { SectionWrapper, ArtistsGrid, TrackList } from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,14 +23,15 @@ const Profile = () => {
       const userPlaylists = await getCurrentUserPlaylists();
       setPlaylists(userPlaylists.data);
 
-      const userTopArtist = await getTopArtists();
-      setTopArtists(userTopArtist.data);
+      const userTopArtists = await getTopArtists();
+      setTopArtists(userTopArtists.data);
+
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
   }, []);
-
-  console.log(topArtists);
 
   return (
     <>
@@ -61,13 +64,20 @@ const Profile = () => {
         </StyledHeader>
       )}
 
-      {topArtists && (
+      {topArtists && topTracks && (
         <main>
           <SectionWrapper
             title="Top artists this month"
             seeAllLink="/top-artists"
           >
             <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+          </SectionWrapper>
+
+          <SectionWrapper
+            title="Top tracks this month"
+            seeAllLink="/top-tracks"
+          >
+            <TrackList tracks={topTracks.items.slice(0, 10)} />
           </SectionWrapper>
         </main>
       )}
