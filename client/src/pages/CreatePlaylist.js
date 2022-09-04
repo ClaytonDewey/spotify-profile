d0822d4695b824d8bbbcdb6a5e52297d1ed7ada2
ClaytonDewey <clay@dryadmedia.com>
2022-09-04 13:02:57 -0500

    Wrap handleSubmit function in useCallback

--- client/src/pages/CreatePlaylist.js
+++ client/src/pages/CreatePlaylist.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { SectionWrapper } from "../components";
 import { StyledHeader, StyledSearchBar } from "../styles";
 import { catchErrors } from "../utils";
@@ -6,25 +6,33 @@ import { search } from "../spotify";
 
 const CreatePlaylist = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState({});
+  const [searchResults, setSearchResults] = useState(undefined);
 
-  useEffect(() => {
-    const fetchData = async (e) => {
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
       const { data } = await search(searchTerm);
       setSearchResults(data);
-    };
+    },
+    [searchTerm]
+  );
 
-    catchErrors(fetchData());
-  });
+  useEffect(() => {
+    catchErrors(handleSubmit());
+  }, [handleSubmit]);
 
-  // console.log(searchResults);
+  console.log(searchTerm);
+
+  if (searchResults) {
+    console.log(searchResults.tracks.items);
+  }
 
   return (
     <>
       <StyledHeader>
         <StyledSearchBar>
           <div className="header__inner">
-            <form>
+            <form onSubmit={handleSubmit}>
               <label htmlFor="spotify-search">
                 Search:{" "}
                 <span className="visually-hidden">
@@ -36,6 +44,7 @@ const CreatePlaylist = () => {
                 placeholder="Enter A Song, Album, or Artist"
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
+              <button type="submit">Search</button>
             </form>
           </div>
         </StyledSearchBar>
@@ -43,24 +52,25 @@ const CreatePlaylist = () => {
 
       <main>
         <SectionWrapper title="Search Results" breadcrumb={true}>
-          {/* {searchResults &&
+          {searchResults &&
             searchResults.tracks.items.map((item) => (
               <div key={item.id}>
                 <img
-                  alt=""
+                  alt={item.album.name}
                   src={item.album.images[0].url}
-                  width={item.album.images[0].width}
-                  height={item.album.images[0].height}
-                  className="mr-1 mb-1"
+                  style={{
+                    width: 120,
+                    height: "auto",
+                  }}
                 />
                 <div className="track__info">
                   <h3>{item.name}</h3>
                   <p>
-                    {item.artist} | {item.album}
+                    {item.artists[0].name} | {item.album.name}
                   </p>
                 </div>
               </div>
-            ))} */}
+            ))}
         </SectionWrapper>
       </main>
     </>
